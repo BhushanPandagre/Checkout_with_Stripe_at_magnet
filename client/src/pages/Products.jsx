b7423d1f420b9d99e6b8d2093a94a9a4e571ee75
aditya <aditya@mb.com>
2025-03-17 16:45:31 +0530

    bhushan

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -90,6 +90,9 @@ const Products = () => {
         "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3",
       price: 9000,
     },
+
+
+   
   ];
 
   const addToCart = (product) => {
